e655c813e0134331da2ae60a9a4624a23e31020a
Lorne Miller <lmiller36@wisc.edu>
2020-06-16 12:19:38 -0500

    Updates

---
+++ precache-manifest.33fc61354c4a66156e0a22299e326917.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e91efcae695fc3304677ce20e66d8677",
+    "url": "/Minesweeper/index.html"
+  },
+  {
+    "revision": "8a99a69d4e7f38e6d3f1",
+    "url": "/Minesweeper/static/css/main.b1302c9f.chunk.css"
+  },
+  {
+    "revision": "d1903e9ce5189bfd33d9",
+    "url": "/Minesweeper/static/js/2.ce7bd5fd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Minesweeper/static/js/2.ce7bd5fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a99a69d4e7f38e6d3f1",
+    "url": "/Minesweeper/static/js/main.c8a1c2f5.chunk.js"
+  },
+  {
+    "revision": "0ad4d6cea6bacab814c3",
+    "url": "/Minesweeper/static/js/runtime-main.c8aca621.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Minesweeper/precache-manifest.2c7a7626da3becdf15e9ce14f114eeef.js"
+  "/Minesweeper/precache-manifest.33fc61354c4a66156e0a22299e326917.js"
 );
 
 self.addEventListener('message', (event) => {
